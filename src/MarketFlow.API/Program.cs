9a336beed30c9feca1b5be8ab24a941cd1d7046f
Eric NGUYEN <eric.nguyen@visiativ.com>
2026-05-06 15:16:46 +0200

    fix: missing authorization in MarketFlow.API

--- src/MarketFlow.API/Program.cs
+++ src/MarketFlow.API/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddOpenApi()
     .AddApplicationLayer()
+    .AddAuthorization()
     .AddInfrastructureLayer(builder.Configuration);
 
 var app = builder.Build();
